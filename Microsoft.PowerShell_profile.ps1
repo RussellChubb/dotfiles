d65118cf705e25fe61e18eaeeb55f054f07c5b53
RussellChubb <148021422+RussellChubb@users.noreply.github.com>
2025-01-16 18:38:52 +1300

    Update Microsoft.PowerShell_profile.ps1
    Removed Whereisutil func, you could use the previous version, but I never seem to use it

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,6 @@
 # Russell's PowerShell Config
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/M365Princess.omp.json' | Invoke-Expression
 
-# Functions
-function whereis ($command) {
-	Get-Command  -Name $command -ErrorAction SilentlyContinue |
-	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
 # Terminal Icons
 Import-Module Terminal-Icons
 
